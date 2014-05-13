5568becbe0c094bd41c03d9143df985c06cf5155
kl <kl@10.66.3.59>
2014-05-13 14:19:51 +0600

    разработка

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -2,8 +2,8 @@
 return array(
     'name'        => 'Международная доставка',
     'description' => 'Стоимость доставки рассчитает Администратор',
-    'icon'        => 'img/courier16.png',
-    'logo'        => 'img/courier.png',
+    'icon'        => 'img/international16.png',
+    'logo'        => 'img/international.png',
     'version'     => '1.0.0',
     'vendor'      => '985310',
 );
--- lib/config/settings.php
+++ lib/config/settings.php
@@ -1,5 +1,11 @@
 <?php
 
 return array(
+    'est_delivery' => array(
+        'title' => 'Срок доставки',
+        'value' => '5-7 рабочих дней',
+        'description' => 'Укажите ориентировочный срок доставки или оставьте пустое поле.',
+        'control_type' => waHtmlControl::INPUT,
+    ),
 );
 //EOF
